029ac0b3e4b5bbf2355e245b64c808456164298d
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2016-10-19 23:58:56 -0400

    with absolute positioning

--- js/sketch.js
+++ js/sketch.js
@@ -15,37 +15,6 @@ cvalenzuela@nyu.edu
 // This is done with the closure below
 console.log("reading sketch...");
 
-var imgs = document.getElementsByTagName('img')
-//var tracker = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
-var tracker = new tracking.ObjectTracker(['face']);
-tracker.setStepSize(1.7);
-
-for (var i = 0; i < imgs.length; i++){
-  var currentTracking = imgs[i];
-
-  tracking.track(currentTracking, tracker);
-
-  tracker.on('track', function(event) {
-    console.log(event.data);
-
-
-    event.data.forEach(function(rect) {
-      window.plot(rect.x, rect.y, rect.width, rect.height);
-    });
-  });
-
-  window.plot = function(x, y, w, h) {
-    var rect = document.createElement('div');
-    document.querySelector('.demo-container').appendChild(rect);
-    rect.classList.add('rect');
-    rect.style.width = w + 'px';
-    rect.style.height = h + 'px';
-    rect.style.left = (currentTracking.offsetLeft + x) + 'px';
-    rect.style.top = (currentTracking.offsetTop + y) + 'px';
-    };
-
-}
-
 /* p5 Object Constructor */
 var sketch = function(p5) {
 
@@ -55,12 +24,41 @@ var sketch = function(p5) {
     /* Canvas */
     var c = p5.createCanvas(p5.windowWidth, p5.windowHeight);
     c.position(0,0);
-    c.style('z-index:999');
     p5.clear();
+
+    /* Define elements to track */
+    var imgs = document.getElementsByTagName('img')
+    //var tracker = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
+    var tracker = new tracking.ObjectTracker(['face']); // Define what to track
+    tracker.setStepSize(1.7); // Set up size of tracker
+
+    /* Look for face recongition in every image in the imgs array */
+    for(var i = 0; i < imgs.length ; i++){
+      var currentImage = imgs[i];
+      tracking.track(imgs[i], tracker);
+    }
+
+    /* Tracker Function when callback is executed */
+    tracker.on('track', function(event) {
+      for (var k = 0; k < event.data.length; k++){
+        p5.fill(255,0,0);
+      }
+
+      event.data.forEach(function(rect) {
+        window.plot(rect.x, rect.y, rect.width, rect.height);
+      });
+
+    });
+
+    /* Draw a rectangle in x,y */
+    window.plot = function(x, y, w, h) {
+      p5.rect((currentImage.offsetLeft + x), (currentImage.offsetTop + y), w, h);
+    };
+
   }
 
   p5.draw = function(){
-    p5.ellipse(p5.mouseX, p5.mouseY, 10,10);
+  //  p5.ellipse(p5.mouseX, p5.mouseY, 10,10);
   }
 }
 
