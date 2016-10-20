b0f6320aa8d2b344c80037f3403aa591907412ce
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2016-10-20 00:44:46 -0400

    added patterns

--- js/sketch.js
+++ js/sketch.js
@@ -25,7 +25,12 @@ var sketch = function(p5) {
     var c = p5.createCanvas(p5.windowWidth, p5.windowHeight);
     c.position(0,0);
     p5.clear();
+    p5.noStroke();
 
+
+  }
+
+  p5.draw = function(){
     /* Define elements to track */
     var imgs = document.getElementsByTagName('img')
     //var tracker = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
@@ -35,30 +40,20 @@ var sketch = function(p5) {
     /* Look for face recongition in every image in the imgs array */
     for(var i = 0; i < imgs.length ; i++){
       var currentImage = imgs[i];
-      tracking.track(imgs[i], tracker);
-    }
+      tracking.track(currentImage, tracker);
 
-    /* Tracker Function when callback is executed */
-    tracker.on('track', function(event) {
-      for (var k = 0; k < event.data.length; k++){
-        p5.fill(255,0,0);
-      }
-
-      event.data.forEach(function(rect) {
-        window.plot(rect.x, rect.y, rect.width, rect.height);
+      tracker.on('track', function(event) {
+        for (var m = 0; m < event.data.length; m++){
+          drawRectangles(event.data[m].x, event.data[m].y, event.data[m].width, event.data[m].height);
+        }
       });
 
-    });
-
-    /* Draw a rectangle in x,y */
-    window.plot = function(x, y, w, h) {
-      p5.rect((currentImage.offsetLeft + x), (currentImage.offsetTop + y), w, h);
-    };
-
-  }
-
-  p5.draw = function(){
-  //  p5.ellipse(p5.mouseX, p5.mouseY, 10,10);
+      /* Draw a rectangle in x,y */
+      drawRectangles = function(x, y, w, h) {
+        p5.fill(p5.random(255),p5.random(255),p5.random(255));
+        p5.rect((currentImage.offsetLeft + x), (currentImage.offsetTop + y), w, h);
+      };
+    }
   }
 }
 
